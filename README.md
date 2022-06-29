c73a003371e8959153e68dc7e185d0502eec8825
安全守护者 <108332453+lixq19888@users.noreply.github.com>
2022-06-29 09:49:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+## 概述
+
+电子邮件是—种用电子手段提供信息交换的通信方式，是互联网应用最广的服务，目前仅提供文本内容的发送。
+
+## 使用说明
+
+Email 在平台上能正常执行的前置条件如下：
+
+1、配置实例，将常用的邮箱服务host、发件人等基础信息填写完成，作为预设，便于后续发邮件时，直接使用；
+
+2、配置动作参数，填写收件人、邮件内容即可发送邮件。
+
+
+## 配置实例
+
+| **参数** |  **参数别名** | **描述** | **必填** | **示例** |
+| --- | --- | --- |  --- | --- |
+| host | SMTP 服务器 |不同类型邮箱，支持的smtp不同 |  是 | smtp.exmail.qq.com |
+| port | 端口 |  发件人邮箱服务端口 | 否 |465 |
+| fromAddress | 发件人 | 发件人邮箱地址 |  否|zhangsan@abt.com |
+| fromAddressAlias | 发件人别名 |发件人别名 |  否 |张三 | 
+| password | 发件人邮箱密码 | 发件人邮箱密码 | 否 | 123456 |
+| debug | 是否打开调试 |  打开调试时，会将运行过程中的日志打印出来，默认关闭 | 否 |false |
+
+#### smtp 服务器说明
+
+| **邮箱类型** | **smtp** |
+| --- | --- | 
+| QQ邮箱 | smtp.qq.com | 
+| 163邮箱 | smtp.163.com |
+| 126邮箱 | smtp.126.com |
+| 安博通邮箱 | smtp.exmail.qq.com | 
+
+#### 发件人邮箱密码说明
+发件人邮箱密码，这里有的是填写密码，部分邮箱填写的是授权码，请注意，目前已知QQ邮箱密码填写的是授权码。
+
+
+## 配置动作
+
+### 1、文本邮件发送
+
+纯文本内容邮件发送。
+
+#### 参数
+| **参数** | **参数别名** | **描述** | **必填** | **示例** |
+| --- | --- | --- | --- |  --- |
+| toAddress | 收件人 | 收件人，支持多个，用逗号隔开 |  是 |zhangsan@abt.com,lisi@abt.com |
+| subject | 主题 |  邮件主题 |是 | 网络安全 |
+| content | 邮件内容 | 邮件内容 | 是 | 这是个好工具 |
+
+#### 返回
+
+```
+send success
+```
+
+
+#### 返回json字段说明
+
+```json
+{
+    "message":"success",
+    "data":"邮件发送状态",
+    "schema":{
+        "type":"STRING"
+    },
+    "debugOutput":"调试信息",
+    "status":"Success"
+}
+```
+
+#### 返回示例
+```json
+{
+    "message":"success",
+    "data":"send success",
+    "schema":{
+        "type":"STRING"
+    },
+    "debugOutput":"subject:测试邮件发送, to_address:123456@qq.com\nmail_host:smtp.exmail.qq.com\n",
+    "status":"Success"
+}
+```
+
+
---
+++ src/app.py
@@ -0,0 +1,114 @@
+"""
+
+"""
+import os
+
+from SoarAction import SoarAction
+from SoarUtils import output_handler
+
+import smtplib
+from email.header import Header
+from email.utils import parseaddr, formataddr
+from email.mime.text import MIMEText
+from SoarDataModel import ResultStatusEnum
+
+APP_NAME = "Email"
+ACTION_LIST = ["TEXT"]
+LogFile = "email.log"
+
+
+class MailServer(SoarAction):
+    __version__ = "1.0.0"
+    subject = ''
+    to_address = ''
+    from_address = ''
+    from_address_alias = ''
+    mail_host = ''
+    password = ''
+    port = 465
+    mail_debug = False
+
+    def __init__(self, app_name, action_list, input_data, action_select):
+        log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), LogFile)
+        super(MailServer, self).__init__(app_name, log_path, action_list, input_data, action_select)
+
+    @output_handler
+    def PING(self):
+        """
+        实例测试
+        :return:
+        """
+        self.params["subject"] = "测试邮件"
+        self.params["content"] = "这是一封测试邮件"
+        self.params["toAddress"] = self.setting_param_dict["fromAddress"]
+        return self.TEXT()
+
+    @output_handler
+    def TEXT(self):
+        """
+        发送简单文本内容邮件
+        :return:
+        """
+        self.subject = self.params["subject"]
+        self.to_address = self.params["toAddress"]
+        self.from_address = self.setting_param_dict["fromAddress"]
+        self.from_address_alias = self.setting_param_dict["fromAddressAlias"]
+        self.mail_host = self.setting_param_dict["host"]
+        self.password = self.setting_param_dict["password"]
+        self.port = self.setting_param_dict["port"]
+        self.mail_debug = self.setting_param_dict["debug"]
+        if self.subject != '' and self.to_address != '' \
+                and self.params["content"] != '' and 0 < self.port < 65535:
+            pass
+        else:
+            return self.get_error_response('param error!')
+        self.logger.info("subject:" + self.subject + ", to_address:" + self.to_address)
+        msg = MIMEText(self.params["content"], 'plain', 'utf-8')
+        if self.from_address_alias:
+            msg['From'] = MailServer.format_address('%s <%s>' % (self.from_address_alias, self.from_address))
+        else:
+            msg['From'] = MailServer.format_address(self.from_address)
+        send_to = MailServer.format_address(self.to_address)
+        msg['To'] = send_to
+        msg['Subject'] = Header(self.subject, 'utf-8').encode()
+
+        try:
+            # 连接到服务器
+            if self.port == 465:
+                smtp_server = smtplib.SMTP_SSL(self.mail_host)
+            else:
+                smtp_server = smtplib.SMTP()
+                smtp_server.connect(self.mail_host, self.port)
+            # 调试日志
+            if str(self.mail_debug).upper() == str('TURE'):
+                smtp_server.set_debuglevel(1)
+            # 登录到邮件服务器
+            smtp_server.login(self.from_address, self.password)
+            # 发送邮件
+            smtp_server.sendmail(self.from_address, msg['To'].split(','), msg.as_string())
+            # 退出
+            smtp_server.quit()
+            return self.get_response_json('send success')
+        except smtplib.SMTPException as e:
+            print('error', e)
+        return self.get_error_response('send fail')
+
+    @staticmethod
+    def format_address(s):
+        """
+        格式化邮件地址
+        :param s:
+        :return:
+        """
+        to_address_str_list = s.split(',')
+        to_address_list = []
+        for to in to_address_str_list:
+            name, address = parseaddr(to)
+            to_address_list.append(formataddr((Header(name, 'utf-8').encode(), address)))
+        return ','.join(to_address_list)
+
+
+if __name__ == '__main__':
+    # input_data = r'%7B%22appName%22%3A%22email%22%2C%22actions%22%3A%5B%7B%22name%22%3A%22TEXT%22%2C%22parameters%22%3A%5B%7B%22name%22%3A%22subject%22%2C%22value%22%3A%22%E9%82%AE%E4%BB%B6%E6%B5%8B%E8%AF%95%E4%B8%BB%E9%A2%981%22%2C%22defaultValue%22%3A%22www.baidu.com%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Atrue%2C%22setting%22%3A%7B%22multiline%22%3Afalse%7D%7D%2C%7B%22name%22%3A%22toAddress%22%2C%22value%22%3A%22fanxiaole%40abtnetworks.com%22%2C%22defaultValue%22%3A%22fanxiaole%40abtnetworks.com%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Atrue%2C%22setting%22%3A%7B%22multiline%22%3Afalse%7D%7D%2C%7B%22name%22%3A%22content%22%2C%22value%22%3A%22%E8%BF%99%E4%B8%AA%E6%98%AF%E9%82%AE%E4%BB%B6%E5%86%85%E5%AE%B91%22%2C%22defaultValue%22%3A%22www.baidu.com%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Atrue%2C%22setting%22%3A%7B%22multiline%22%3Afalse%7D%7D%2C%7B%22name%22%3A%22host%22%2C%22value%22%3A%22%22%2C%22defaultValue%22%3A%22smtp.qq.com%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Afalse%7D%2C%7B%22name%22%3A%22port%22%2C%22value%22%3A%22%22%2C%22defaultValue%22%3A%22465%22%2C%22schema%22%3A%7B%22type%22%3A%22INTEGER%22%7D%2C%22required%22%3Afalse%7D%2C%7B%22name%22%3A%22fromAddress%22%2C%22value%22%3A%22%22%2C%22defaultValue%22%3A%221964645988%40qq.com%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Afalse%7D%2C%7B%22name%22%3A%22fromAddressAlias%22%2C%22value%22%3A%22%E5%AE%89%E5%8D%9A%E9%80%9A%E7%B3%BB%E7%BB%9F%E9%82%AE%E4%BB%B6%22%2C%22defaultValue%22%3A%22xxx%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Afalse%7D%2C%7B%22name%22%3A%22password%22%2C%22value%22%3A%22%22%2C%22defaultValue%22%3A%22kpnkmqtronvmcefd%22%2C%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22required%22%3Afalse%7D%2C%7B%22name%22%3A%22debug%22%2C%22value%22%3A%22%22%2C%22defaultValue%22%3A%22False%22%2C%22schema%22%3A%7B%22type%22%3A%22BOOLEAN%22%7D%2C%22required%22%3Afalse%7D%5D%2C%22returns%22%3A%7B%22schema%22%3A%7B%22type%22%3A%22STRING%22%7D%2C%22example%22%3A%22success%22%2C%22description%22%3A%22XXXXXX%22%7D%7D%5D%7D'
+    app = MailServer(APP_NAME, ACTION_LIST, None, None)
+    app.do_action()
